0ff508b8866b7aa25b16439a5652e9a8b2ad206b
Suraj <suraj@192.168.0.115>
2026-02-16 10:13:25 +0530

    bugs

--- lib/core/endpoitns.dart
+++ lib/core/endpoitns.dart
@@ -5,11 +5,11 @@ class AppEndpoints {
   // static const String socketUrl = "https://padelstagingmobileapi.swootapp.com";
 
    /// -----------------------New Live URL-------------------------------------------
-  // static const String baseUrl = "https://apimobile.swootapp.com/api/customer/";
-  // static const String socketUrl = "https://apimobile.swootapp.com";
+  static const String baseUrl = "https://apimobile.swootapp.com/api/customer/";
+  static const String socketUrl = "https://apimobile.swootapp.com";
   ///-----------------------Local URL-------------------------------------------
-  static const String baseUrl = "http://192.168.0.121:5070/api/customer/";
-  static const String socketUrl = "http://192.168.0.121:5070";
+  // static const String baseUrl = "http://192.168.0.121:5070/api/customer/";
+  // static const String socketUrl = "http://192.168.0.121:5070";
 
 
   // static const String login = "${baseUrl}customerLogin";
@@ -105,7 +105,7 @@ class AppEndpoints {
 }
 class PaymentConfig {
   // Toggle this when switching environment
-  static const bool isLive = false;
+  static const bool isLive = true;
 
   // Test Key
   static const String _testKeyId = 'rzp_test_RtRFaVPUzoUtkG';
