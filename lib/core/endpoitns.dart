8f088cb34c3702e33d47c252e54a60fc8ba5cb8b
Suraj <suraj@192.168.0.126>
2026-02-23 10:08:09 +0530

    pull

--- lib/core/endpoitns.dart
+++ lib/core/endpoitns.dart
@@ -1,14 +1,14 @@
 class AppEndpoints {
   AppEndpoints._();
   /// -----------------------Staging URL-------------------------------------------
-  // static const String baseUrl = "https://padelstagingmobileapi.swootapp.com/api/customer/";
-  // static const String socketUrl = "https://padelstagingmobileapi.swootapp.com";
+  static const String baseUrl = "https://padelstagingmobileapi.swootapp.com/api/customer/";
+  static const String socketUrl = "https://padelstagingmobileapi.swootapp.com";
    /// -----------------------New Live URL-------------------------------------------
   // static const String baseUrl = "https://apimobile.swootapp.com/api/customer/";
   // static const String socketUrl = "https://apimobile.swootapp.com";
   ///-----------------------Local URL-------------------------------------------
-  static const String baseUrl = "http://192.168.0.121:5070/api/customer/";
-  static const String socketUrl = "http://192.168.0.121:5070";
+  // static const String baseUrl = "http://192.168.0.121:5070/api/customer/";
+  // static const String socketUrl = "http://192.168.0.121:5070";
 
 
   // static const String login = "${baseUrl}customerLogin";
