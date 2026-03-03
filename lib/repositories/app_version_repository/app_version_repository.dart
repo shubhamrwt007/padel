4e0023834daa424b00a17c6c2636efbd409b797a
Suraj <suraj@192.168.1.39>
2026-03-03 18:29:49 +0530

    90 minutes flow

--- lib/repositories/app_version_repository/app_version_repository.dart
+++ lib/repositories/app_version_repository/app_version_repository.dart
@@ -7,31 +7,31 @@ import 'package:dio/dio.dart';
 class AppVersionRepository {
   final DioClient dioClient = DioClient();
 
-  Future<AppVersionModel> checkAppVersion({required Map<String, dynamic> body}) async {
-    try {
-      CustomLogger.logMessage(msg: "APP VERSION BODY:-> $body", level: LogLevel.info);
-      final response = await dioClient.post(
-        AppEndpoints.appVersions,
-        data: body,
-        options: Options(headers: {'Authorization': ''}),
-      );
-
-      if (response.statusCode == 200) {
-        CustomLogger.logMessage(
-          msg: "App version check successful: ${response.data}",
-          level: LogLevel.info,
-        );
-        return AppVersionModel.fromJson(response.data);
-      } else {
-        throw Exception("App version check failed with status code: ${response.statusCode}");
-      }
-    } catch (e, st) {
-      CustomLogger.logMessage(
-        msg: "App version check failed with error: ${e.toString()}",
-        level: LogLevel.error,
-        st: st,
-      );
-      rethrow;
-    }
-  }
+  // Future<AppVersionModel> checkAppVersion({required Map<String, dynamic> body}) async {
+  //   try {
+  //     CustomLogger.logMessage(msg: "APP VERSION BODY:-> $body", level: LogLevel.info);
+  //     final response = await dioClient.post(
+  //       AppEndpoints.appVersions,
+  //       data: body,
+  //       options: Options(headers: {'Authorization': ''}),
+  //     );
+  //
+  //     if (response.statusCode == 200) {
+  //       CustomLogger.logMessage(
+  //         msg: "App version check successful: ${response.data}",
+  //         level: LogLevel.info,
+  //       );
+  //       return AppVersionModel.fromJson(response.data);
+  //     } else {
+  //       throw Exception("App version check failed with status code: ${response.statusCode}");
+  //     }
+  //   } catch (e, st) {
+  //     CustomLogger.logMessage(
+  //       msg: "App version check failed with error: ${e.toString()}",
+  //       level: LogLevel.error,
+  //       st: st,
+  //     );
+  //     rethrow;
+  //   }
+  // }
 }
