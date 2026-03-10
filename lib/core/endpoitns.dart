2aaddfe617b084ff2f0b1f7aceb808e55ec354c4
Suraj <suraj@192.168.0.108>
2026-03-10 10:18:59 +0530

    soket on scoreboard

--- lib/core/endpoitns.dart
+++ lib/core/endpoitns.dart
@@ -3,15 +3,12 @@ class AppEndpoints {
   /// -----------------------Staging URL-------------------------------------------
   // static const String baseUrl = "https://padelstagingmobileapi.swootapp.com/api/customer/";
   // static const String socketUrl = "https://padelstagingmobileapi.swootapp.com";
-
    /// -----------------------New Live URL--------------------------------------
   // static const String baseUrl = "https://apimobile.swootapp.com/api/customer/";
   // static const String socketUrl = "https://apimobile.swootapp.com";
   ///-----------------------Local URL-------------------------------------------
-
   static const String baseUrl = "http://192.168.0.126:5070/api/customer/";
   static const String socketUrl = "http://192.168.0.126:5070";
-
   // static const String login = "${baseUrl}customerLogin";
   static const String login = "${baseUrl}customerLoginByPhoneNumber";
   static const String logout = "${baseUrl}logout";
@@ -50,7 +47,6 @@ class AppEndpoints {
   ///Review---------------------------------------------------------------------
   static const String getReview = "${baseUrl}court/review/getReviews";
   static const String createReview = "${baseUrl}court/review/saveCustomerReview";
-
   ///Open Matches---------------------------------------------------------------
   static const String createMatches = "${baseUrl}court/openmatch/createOpenMatch";
   static const String createOpenMatchSlotBookOnly = "${baseUrl}court/openmatch/create-openMatchOnly";
@@ -87,7 +83,6 @@ class AppEndpoints {
   static const String getWallet = "${baseUrl}wallet/getWallet";
   static const String testWalletCreate = "${baseUrl}wallet/order";
   static const String appVersions= "${baseUrl}app-version/check";
-
   ///LeaderBoard----------------------------------------------------------------
   static const String getLeaderBoard = "${baseUrl}court/leaderBoard/player";
   static const String removePlayer = "${baseUrl}court/openmatch/removePlayerFromMatch";
