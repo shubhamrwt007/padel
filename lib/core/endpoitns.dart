1dd22abdfddda0d2f57f2932a3162a0e97015de8
Suraj <suraj@192.168.0.118>
2026-03-16 19:01:04 +0530

    90 minutes

--- lib/core/endpoitns.dart
+++ lib/core/endpoitns.dart
@@ -1,14 +1,14 @@
 class AppEndpoints {
   AppEndpoints._();
   /// -----------------------Staging URL----------------------------------------
-  // static const String baseUrl = "https://padelstagingmobileapi.swootapp.com/api/customer/";
-  // static const String socketUrl = "https://padelstagingmobileapi.swootapp.com";
+  static const String baseUrl = "https://padelstagingmobileapi.swootapp.com/api/customer/";
+  static const String socketUrl = "https://padelstagingmobileapi.swootapp.com";
    /// -----------------------New Live URL--------------------------------------
   // static const String baseUrl = "https://apimobile.swootapp.com/api/customer/";
   // static const String socketUrl = "https://apimobile.swootapp.com";
   ///-----------------------Local URL-------------------------------------------
-  static const String baseUrl = "http://192.168.1.23:5070/api/customer/";
-  static const String socketUrl = "http://192.168.1.23:5070";
+  // static const String baseUrl = "http://192.168.1.23:5070/api/customer/";
+  // static const String socketUrl = "http://192.168.1.23:5070";
 
 
   // static const String login = "${baseUrl}customerLogin";
@@ -100,7 +100,7 @@ class AppEndpoints {
 }
 class PaymentConfig {
   // Toggle this when switching environment
-  static const bool isLive = true;
+  static const bool isLive = false;
   // Test Key
   static const String _testKeyId = 'rzp_test_RtRFaVPUzoUtkG';
   // Live Key
