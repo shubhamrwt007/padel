706056467048c8d60ac940df5adee9236b186370
Abhishek Balyani <abhishekbalyani@192.168.1.14>
2026-03-30 10:09:45 +0530

    -

--- lib/core/endpoitns.dart
+++ lib/core/endpoitns.dart
@@ -2,14 +2,14 @@ class AppEndpoints {
   AppEndpoints._();
 
   /// -----------------------Staging URL-------------------------------------------
-  static const String base = "https://padelstagingmobileapi.swootapp.com/api/";
-  static const String socketUrl = "https://padelstagingmobileapi.swootapp.com";
+  // static const String base = "https://padelstagingmobileapi.swootapp.com/api/";
+  // static const String socketUrl = "https://padelstagingmobileapi.swootapp.com";
    /// -----------------------New Live URL--------------------------------------
   // static const String base = "https://apimobile.swootapp.com/api/";
   // static const String socketUrl = "https://apimobile.swootapp.com";
   ///-----------------------Local URL-------------------------------------------
-  // static const String base = "http://192.168.0.109:5080/api/";
-  // static const String socketUrl = "http://192.168.0.109:5080";
+  static const String base = "http://192.168.1.20:5070/api/";
+  static const String socketUrl = "http://192.168.1.20:5070";
 
   static const String baseUrl = "${base}customer/";
 
